58df14e6697e5f45784f65d9ddd04f2adbec4a24
oshan mitkari <oshanmitkari778@gmail.com>
2026-02-07 00:47:16 +0530

    3rd error

--- app/src/main/java/com/example/summereasy/LoginActivity.java
+++ app/src/main/java/com/example/summereasy/LoginActivity.java
@@ -14,7 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     LottieAnimationView laView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login_page);
@@ -30,4 +31,5 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
     }
+    problem in code
 }
